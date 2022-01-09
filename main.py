ad21d02f1677b5c408935484131928ef0ca0fe0b
Fredohm <fredwaucquez@hotmail.fr>
2022-01-09 13:51:34 +0100

    Add for loops for ingredients and improve process_coins

--- main.py
+++ main.py
@@ -17,35 +17,30 @@ def check_resources(user_choice):
     """function to check resources, print message if not enough and return boolean"""
     # check ingredients against user_choice
     is_enough_ingredients = True
-    has_milk = check_type(user_choice)
-
-    if MENU[user_choice]["ingredients"]["water"] <= resources["water"]:
-        if MENU[user_choice]["ingredients"]["coffee"] <= resources["coffee"]:
-            if has_milk:
-                if MENU[user_choice]["ingredients"]["milk"] <= resources["milk"]:
-                    resources["milk"] -= MENU[user_choice]["ingredients"]["milk"]
-                else:
-                    print("Sorry, there is not enough milk.")
-                    is_enough_ingredients = False
-            resources["water"] -= MENU[user_choice]["ingredients"]["water"]
-            resources["coffee"] -= MENU[user_choice]["ingredients"]["coffee"]
-        else:
-            print("Sorry, there is not enough coffee.")
+    for ingredient in MENU[user_choice]["ingredients"]:
+        if MENU[user_choice]["ingredients"][ingredient] > resources[ingredient]:
+            print(f"Sorry, there is not enough {ingredient}")
             is_enough_ingredients = False
-    else:
-        print("Sorry, there is not enough water.")
-        is_enough_ingredients = False
 
     return is_enough_ingredients
 
 
-def process_coins():
+def process_coins(cost):
     """function to process coins. Return float"""
-    quarters = int(input("Quarters: $"))
-    dimes = int(input("Dimes: $"))
-    nickles = int(input("Nickels: $"))
-    pennies = int(input("Pennies: $"))
-    return 0.25 * quarters + 0.1 * dimes + 0.05 * nickles + 0.01 * pennies
+    print("Please insert coins.")
+    total_amount = 0
+    quarters = int(input("Quarters: $")) * 0.25
+    total_amount = quarters
+    if total_amount < cost:
+        dimes = int(input("Dimes: $")) * 0.1
+        total_amount += dimes
+        if total_amount < cost:
+            nickles = int(input("Nickels: $")) * 0.05
+            total_amount += nickles
+            if total_amount < cost:
+                pennies = int(input("Pennies: $")) * 0.01
+                total_amount += pennies
+    return total_amount
 
 
 def check_transaction(given_amount, coffee_price):
@@ -65,33 +60,21 @@ def make_coffee():
     is_off = False
     total_money = 0
     while not is_off:
-        user_choice = input("What would you like? (espresso/latte/cappuccino):\n")
-        if user_choice == "espresso":
-            if check_resources(user_choice):
-                given_amount = process_coins()
-                if check_transaction(given_amount, MENU[user_choice]["cost"]):
-                    print(f"Here is your {user_choice}. Enjoy!")
-                    total_money += MENU[user_choice]["cost"]
-        elif user_choice == "latte":
-            if check_resources(user_choice):
-                given_amount = process_coins()
-                if check_transaction(given_amount, MENU[user_choice]["cost"]):
-                    print(f"Here is your {user_choice}. Enjoy!")
-                    total_money += MENU[user_choice]["cost"]
-        elif user_choice == "cappuccino":
-            if check_resources(user_choice):
-                given_amount = process_coins()
-                if check_transaction(given_amount, MENU[user_choice]["cost"]):
-                    print(f"Here is your {user_choice}. Enjoy!")
-                    total_money += MENU[user_choice]["cost"]
+        user_choice = input("What would you like? (espresso/latte/cappuccino): ")
+        if user_choice == "off":
+            is_off = True
+            print(f"Total money collected: ${total_money}")
+            print("Worst coffee machine stopped!")
         elif user_choice == "report":
             print(report(total_money))
         else:
-            if user_choice == "off":
-                is_off = True
-                print("Worst coffee machine stopped!")
-            else:
-                return total_money
+            if check_resources(user_choice):
+                given_amount = process_coins(MENU[user_choice]["cost"])
+                if check_transaction(given_amount, MENU[user_choice]["cost"]):
+                    for ingredient in MENU[user_choice]["ingredients"]:
+                        resources[ingredient] -= MENU[user_choice]["ingredients"][ingredient]
+                    print(f"Here is your {user_choice}. Enjoy!\n")
+                    total_money += MENU[user_choice]["cost"]
 
 
 print("Welcome to the worst coffee machine!\n")
